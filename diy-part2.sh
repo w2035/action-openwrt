65a7bff352f66112822e9b796c99b9ee1bdd62f6
w2035 <42054399+w2035@users.noreply.github.com>
2024-11-02 13:27:23 +1030

    remove speedtest web

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/luci/applications/luci-app-mosdns 
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+rm -rf feeds/smpackage/speedtest-web
